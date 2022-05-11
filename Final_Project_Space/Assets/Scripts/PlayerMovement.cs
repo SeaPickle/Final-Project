eba916c73175f1b0a0fdd822c8b19bd8ff9ec339
Gonzalez <707258@student.dusd.net>
2022-05-11 10:57:35 -0700

    Follow Mouse

--- Final_Project_Space/Assets/Scripts/PlayerMovement.cs
+++ Final_Project_Space/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 20f;
+    public Camera cam;
+
+    Vector2 mousePos;
 
     Rigidbody2D rb;
     void Start()
@@ -18,10 +21,14 @@ public class PlayerMovement : MonoBehaviour
         float xin = Input.GetAxisRaw("Horizontal");
         float yin = Input.GetAxisRaw("Vertical");
         rb.MovePosition(new Vector2(p.x, p.y) + new Vector2(xin, yin) * moveSpeed * Time.deltaTime);
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }

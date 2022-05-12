e72a199b0078f5cf9fcb5cf73ccca08e8cffcb0c
Gonzalez <707258@student.dusd.net>
2022-05-12 10:55:07 -0700

    Finishing up movement & starting on shooting

--- Final_Project_Space/Assets/Scripts/PlayerMovement.cs
+++ Final_Project_Space/Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 20f;
+    public float moveSpeed = 5f;
+
     public Camera cam;
 
+    Vector2 movement;
     Vector2 mousePos;
 
     Rigidbody2D rb;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,16 +20,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        Vector3 p = transform.position;
-        float xin = Input.GetAxisRaw("Horizontal");
-        float yin = Input.GetAxisRaw("Vertical");
-        rb.MovePosition(new Vector2(p.x, p.y) + new Vector2(xin, yin) * moveSpeed * Time.deltaTime);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;

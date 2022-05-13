0bc0d6b4caf40a5b26f9f5898a506a619e61e70e
Viskade <gianlucagonzalez4@gmail.com>
2022-05-12 18:05:03 -0700

    Main Shooting
    Finished shooting mechanics for player, added assets from Unity Store for effects: Pixel FX - Fire

--- Final_Project_Space/Assets/Scripts/Shooting.cs
+++ Final_Project_Space/Assets/Scripts/Shooting.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Transform Barrel;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, Barrel.position, Barrel.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(Barrel.up * bulletForce, ForceMode2D.Impulse);
     }
 }
